003740b46709c17162483ef8886a60e4e496c835
José Joaquim <josejoaquimrizzo@hotmail.com>
2020-07-19 18:03:45 +0100

    inicio (commit on 'zedev')

---
+++ database/insert.php
@@ -0,0 +1,17 @@
+<?php
+include ('bd.php');
+try{
+$user_name=$_POST['user_name'];
+$user_email=$_POST['email'];
+$password=$_POST['password'];
+$contact=$_POST['contact'];
+$adress=$_POST['adress'];
+$sql = "INSERT INTO users ( name, email, password, contact, adress) VALUES ('$user_name','$email','$password',$contact',$adress')";
+$conn->exec($sql);
+echo "aluno inserido!<br>";
+}catch(PDOException $e){
+echo $sql . "<br>" . $e->getMessage();
+}
+echo "<a href=\"index.html\">Voltar à página inicial</a>";
+$conn = null;
+?>
---
+++ database/lista.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>Lista de alunos</h1>
+<a href="insert.php">Inserir novo aluno</a><hr>
+<?php include('selectall.php'); ?>
+</body>
+</html>

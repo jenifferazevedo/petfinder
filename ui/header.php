e9b9dee274c376b9d4e2e5d2a67b6da27b53f938
Jeniffer <jeniqf@gmail.com>
2020-07-19 17:59:08 +0100

    header update

--- ui/header.php
+++ ui/header.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;600;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/mystyle.css">
     <title><?php 
       if(isset($_GET['p'])) {
         echo 'PETFINDER - '.$_GET['p'];
@@ -16,6 +17,9 @@
     </title>
 </head>
 <body>
-<div>
+  <header>
+    <div id="header">
+      <img src="./img/petfinder_logo_clara.svg" alt="Logo Petfinder">
+    </div>
+  </header>
   <?php include("nav.php")?>
-</div>

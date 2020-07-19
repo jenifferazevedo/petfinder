065bc661e7aa86282718b31318abc3e687798be8
Jeniffer <jeniqf@gmail.com>
2020-07-19 17:59:26 +0100

    Nav update

--- ui/nav.php
+++ ui/nav.php
@@ -1,28 +1,58 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+  <a class="navbar-brand" href="#"><img src="./img/petfinder_logo.svg" alt="Logo Petfinder"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    <ul class="navbar-nav ml-auto">
+      <?php 
+        if(isset($_GET['p']) && $_GET['p'] == 'Home') {
+          echo '<li class="nav-item active">';
+        } else if (isset($_GET['p']) && $_GET['p'] != 'Home') {
+          echo '<li class="nav-item">';
+        }
+         else {
+          echo '<li class="nav-item active">';
+        };
+      ?>
+        <a class="nav-link" href="index.php?p=Home">Home</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+      <?php 
+        if(isset($_GET['p']) && $_GET['p'] == 'SobreNos') {
+          echo '<li class="nav-item active">';
+        } else {
+          echo '<li class="nav-item">';
+        };
+      ?>
+        <a class="nav-link" href="index.php?p=SobreNos">Sobre Nós</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+      <?php 
+        if(isset($_GET['p']) && $_GET['p'] == 'Adocao') {
+          echo '<li class="nav-item active">';
+        } else {
+          echo '<li class="nav-item">';
+        };
+      ?>
+        <a class="nav-link" href="index.php?p=Adocao">Adoção</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+      <?php 
+        if(isset($_GET['p']) && $_GET['p'] == 'Contacto') {
+          echo '<li class="nav-item active">';
+        } else {
+          echo '<li class="nav-item">';
+        };
+      ?>
+        <a class="nav-link" href="index.php?p=Contacto">Contacto</a>
+      </li>
+      <?php 
+        if(isset($_GET['p']) && $_GET['p'] == 'Login') {
+          echo '<li class="nav-item active">';
+        } else {
+          echo '<li class="nav-item">';
+        };
+      ?>
+        <a class="nav-link" href="index.php?p=Login">Login</a>
       </li>
     </ul>
   </div>

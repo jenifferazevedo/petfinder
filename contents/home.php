0f88bf99f767979ba54e7c4f4e85bae4df3e65b6
Jeniffer <jeniqf@gmail.com>
2020-07-19 17:58:48 +0100

    Testing the pages

---
+++ contents/home.php
@@ -0,0 +1,20 @@
+<h1>home</h1>
+<p style="font-size: 30px;">Ullamco proident elit sit commodo commodo cupidatat eu velit qui dolore sunt
+ aliquip reprehenderit do. Velit laboris sunt exercitation Lorem incididunt deserunt 
+ nostrud occaecat officia amet tempor proident. Ea elit sint labore esse nisi eiusmod 
+ aliquip laborum do voluptate cillum sunt Lorem. Fugiat sit enim qui adipisicing consectetur 
+ sit cupidatat voluptate proident pariatur eu. Occaecat dolore incididunt reprehenderit nostrud sit. 
+ Reprehenderit ea occaecat sint ut mollit tempor in fugiat ut excepteur dolor deserunt.
+ Ullamco proident elit sit commodo commodo cupidatat eu velit qui dolore sunt
+ aliquip reprehenderit do. Velit laboris sunt exercitation Lorem incididunt deserunt 
+ nostrud occaecat officia amet tempor proident. Ea elit sint labore esse nisi eiusmod 
+ aliquip laborum do voluptate cillum sunt Lorem. Fugiat sit enim qui adipisicing consectetur 
+ sit cupidatat voluptate proident pariatur eu. Occaecat dolore incididunt reprehenderit nostrud sit. 
+ Reprehenderit ea occaecat sint ut mollit tempor in fugiat ut excepteur dolor deserunt
+ Reprehenderit ea occaecat sint ut mollit tempor in fugiat ut excepteur dolor deserunt.
+ Ullamco proident elit sit commodo commodo cupidatat eu velit qui dolore sunt
+ aliquip reprehenderit do. Velit laboris sunt exercitation Lorem incididunt deserunt 
+ nostrud occaecat officia amet tempor proident. Ea elit sint labore esse nisi eiusmod 
+ aliquip laborum do voluptate cillum sunt Lorem. Fugiat sit enim qui adipisicing consectetur 
+ sit cupidatat voluptate proident pariatur eu. Occaecat dolore incididunt reprehenderit nostrud sit. 
+ Reprehenderit ea occaecat sint ut mollit tempor in fugiat ut excepteur dolor deserunt  </p>

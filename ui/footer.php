b24c2a5f33925968a9999d22ed0bd669abf798e9
Jeniffer <jeniqf@gmail.com>
2020-07-19 15:46:01 +0100

    footer

---
+++ ui/footer.php
@@ -0,0 +1,9 @@
+  <div>
+    <p>Desenvolvido por Equipa JJ 
+      <?php if(isset($_GET['p'])) 
+            echo '<a style="text-decoration: none; color: aquamarine; font-weight: bold; padding-left: 10px" href="index.php"><i class="fa fa-chevron-left" aria-hidden="true"></i> Voltar à página inicial</a>'
+      ?>
+    </p>
+  </div>
+</body>
+</html>

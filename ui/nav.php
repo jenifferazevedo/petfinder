042e79db377828841d9235fc4a244c3b69aac87d
Jeniffer <jeniqf@gmail.com>
2020-07-22 01:01:30 +0100

    Fixed the nav for each type of user
    Need to fix the user image on nav.

--- ui/nav.php
+++ ui/nav.php
@@ -4,28 +4,24 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <?php 
-  if(isset($logado, $tipo) && $tipo == 1): ?>
-    <?php 
-    $logado=$_POST['logado'];
-    $tipo=$_POST['tipo'];
-    $user_foto = $_POST['user_foto'];?>
+  <?php
+  if (isset($_SESSION['petfinder-user'])): ?>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav ml-auto">
         <?php 
-          if(isset($_GET['p']) && $_GET['p'] == 'Home') {
+          if(isset($_GET['s']) && $_GET['s'] == 'Home') {
             echo '<li class="nav-item active">';
-          } else if (isset($_GET['p']) && $_GET['p'] != 'Home') {
+          } else if (isset($_GET['s']) && $_GET['s'] != 'Home') {
             echo '<li class="nav-item">';
           }
           else {
             echo '<li class="nav-item active">';
           };
         ?>
-          <a class="nav-link" href="index.php?p=Home">Home</a>
+          <a class="nav-link" href="index.php?s=Home">Home</a>
         </li>
         <?php 
-          if(isset($_GET['p']) && $_GET['p'] == 'SobreNos') {
+          if(isset($_GET['s']) && $_GET['s'] == 'SobreNos') {
             echo '<li class="nav-item active">';
           } else {
             echo '<li class="nav-item">';
@@ -34,44 +30,43 @@
           <a class="nav-link" href="index.php?p=SobreNos">Sobre Nós</a>
         </li>
         <?php 
-          if(isset($_GET['p']) && $_GET['p'] == 'Adocao') {
+          if(isset($_GET['s']) && $_GET['s'] == 'Adocao') {
             echo '<li class="nav-item active">';
           } else {
             echo '<li class="nav-item">';
           };
         ?>
-          <a class="nav-link" href="index.php?p=Adocao">Adoção</a>
+          <a class="nav-link" href="index.php?s=Adocao">Adoção</a>
         </li>
         <?php 
-          if(isset($_GET['p']) && $_GET['p'] == 'Contacto') {
+          if(isset($_GET['s']) && $_GET['s'] == 'Contacto') {
             echo '<li class="nav-item active">';
           } else {
             echo '<li class="nav-item">';
           };
         ?>
-          <a class="nav-link" href="index.php?p=Contacto">Contacto</a>
+          <a class="nav-link" href="index.php?s=Contacto">Contacto</a>
         </li>
         <?php 
-          if(isset($_GET['p']) && $_GET['p'] == 'Usuario') {
+          if(isset($_GET['s']) && $_GET['s'] == 'Usuario') {
             echo '<li class="nav-item dropdown active">';
           } else {
             echo '<li class="nav-item dropdown">';
           };
         ?>
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <?php echo'<div class="user-foto"><img src="'.$user_foto.'" alt="Foto Usuário"></div>'; ?>
+            <?php echo'<img src="" alt="Foto Usuário">'; ?>
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Perfil</a>
-            <a class="dropdown-item" href="#">Cadastro Pet</a>
-            <a class="dropdown-item" href="#">Favoritos</a>
+            <a class="dropdown-item" href="index.php?s=Perfil">Perfil</a>
+            <a class="dropdown-item" href="index.php?s=CadastroPet">Cadastro Pet</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Logout</a>
+            <a class="dropdown-item" href="./database/logout.php">Logout</a>
           </div>        
         </li>
       </ul>
     </div>
-  <?php /*elseif(isset($logado, $tipo) && $tipo == 0):*/ elseif(isset($_GET['p']) && $_GET['p'] == 'Admin' ): ?>
+  <?php elseif(isset($_SESSION['petfinder-admin'])): ?>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav ml-auto">
         <?php if(isset($_GET['pg']) && $_GET['pg'] == 'Home') {
@@ -83,7 +78,7 @@
               echo '<li class="nav-item active">';
             };
             ?>
-            <a class="nav-link" href="index.php?p=Admin&pg=Home">Home</a>
+            <a class="nav-link" href="index.php?pg=Home">Home</a>
           </li>
           <?php 
             if(isset($_GET['p']) && $_GET['p'] == 'Tables') {
@@ -96,8 +91,8 @@
               Tables
             </a>
             <div class="dropdown-menu" style="left: auto; right: 0px;" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="index.php?p=Admin&pg=TableUser">Users</a>
-              <a class="dropdown-item" href="index.php?p=Admin&pg=TablePet">Pets</a>
+              <a class="dropdown-item" href="index.php?pg=TableUser">Users</a>
+              <a class="dropdown-item" href="index.php?pg=TablePet">Pets</a>
             </div> 
           </li>
           <?php 
@@ -111,14 +106,12 @@
             <i class="fa fa-user-circle-o" aria-hidden="true" style="font-size: 1.5em"></i>
             </a>
             <div class="dropdown-menu" style="left: auto; right: 0px;" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="index.php?p=Admin&pg=Perfil">Perfil</a>
+              <a class="dropdown-item" href="index.php?pg=Perfil">Perfil</a>
               <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Logout</a>
+              <a class="dropdown-item" href="./database/logout.php">Logout</a>
             </div>   
         </ul>
       </div>
-          
-
   <?php else: ?>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav ml-auto">

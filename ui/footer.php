9bbc6e83a38098dabbaae0e61c3c206b1d08e9b3
Jeniffer <jeniqf@gmail.com>
2020-08-11 23:53:55 +0100

    Add 'myscript.js'

--- ui/footer.php
+++ ui/footer.php
@@ -1,14 +1,14 @@
   <div id="footer" class="fixed-bottom d-flex align-items-center">
     <p class="m-0 pl-2">Desenvolvido por Equipa JJ 
       <?php if(isset($_GET['p'])) 
-            echo '<a class="voltar" href="index.php"><i class="fa fa-chevron-left" aria-hidden="true"></i> Voltar à página inicial</a>'
+        echo '<a class="voltar" href="index.php"><i class="fa fa-chevron-left" aria-hidden="true"></i> Voltar à página inicial</a>'
       ?>
     </p>
     <a href="https://github.com/jenifferazevedo/petfinder"><i class="fa fa-github" aria-hidden="true"></i></a>
     <script src="./script/jquery-3.5.1.slim.min.js"></script>
     <script src="./script/popper.min.js"></script>
     <script src="./script/bootstrap.min.js"></script>
-
+    <script src="./script/myscript.js"></script>
   </div>
 </body>
 </html>

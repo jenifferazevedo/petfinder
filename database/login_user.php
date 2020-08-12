20c8b6d9beb89205c3fbbf74369cbb38869eaa92
Jeniffer <jeniqf@gmail.com>
2020-08-12 23:22:49 +0100

    Login user changes

--- database/login_user.php
+++ database/login_user.php
@@ -5,15 +5,15 @@
     $query->execute(array($_POST['email'], $_POST['password']));
     if($query->rowCount()) {
       $user = $query->fetchAll(PDO::FETCH_ASSOC)[0];
-      if($user['tipo']) {
+      if($user['tipo'] == 1) {
         session_start();
         $_SESSION['petfinder-admin'] = array($user['user_id'], $user['name'], $user['email'], $user['contact'], $user['city']);
-        header('Location: ../index.php');
+        header('Location: ../index.php?pg=Home');
       }
       else{
         session_start();
         $_SESSION['petfinder-user'] = array($user['user_id'], $user['name'], $user['email'], $user['contact'], $user['city']);
-        echo "<script>window.location = '../index.php';</script>";
+        header('Location: ../index.php?s=Home');
       }
     }
     else {

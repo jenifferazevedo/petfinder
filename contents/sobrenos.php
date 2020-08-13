26018115e5d50614de2ec5ab20995b65ee968587
Jeniffer <jeniqf@gmail.com>
2020-08-13 01:04:09 +0100

    About us changes

---
+++ contents/sobrenos.php
@@ -0,0 +1,46 @@
+<div class="container p-5">
+  <div class="row leftScroll">
+    <div class="col-12">
+      <h1>Uma acção para um novo mundo!</h1>
+      <div class="row pt-5">
+        <div class="col-sm-7 col-12">
+          <p>
+            O Petfinder tem como objetivo ajudar animais a encontrarem um lar através de uma plataforma interativa,
+            assim queremos:
+            <ul>
+              <li>Ajudar as associações a encontrarem um lar para os seus animais</li>
+              <li>Ajudar as pessoas a divulgarem animais abandonados</li>
+              <li>Estimular a adopção de animais</li>
+            </ul> 
+            Ajude o animal que se encontra abandonado na sua rua a encontrar um lar cheio de amor.
+            Navegue na nossa plataforma e se tiver dúvidas é só entrar em <a href="index.php?p=Contacto">contacto</a>.
+          </p> 
+        </div>
+        <div class="col-sm-5 col-12">
+          <img class="w-100" src="./img/pets3.png" alt="" srcset="">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row pt-5 rightScroll">
+    <div class="col-12">
+      <h2 style="color:#E8680C">Nossa Equipa</h2>
+      <div class="row justify-content-center pt-4">
+        <div class="col-12 col-sm-4 text-center">
+          <div>
+            <img class="rounded-circle w-100" style="max-width: 200px" src="https://media-exp1.licdn.com/dms/image/C5603AQHK9VYPbm9BJw/profile-displayphoto-shrink_200_200/0?e=1602720000&v=beta&t=FO9v3YC7g5sSCUJVSsF_tG_cX-M-g0RvX6ef4o0TBY4" alt="">
+          </div>
+          <h3>Jeniffer Azevedo</h3>
+          <p>Web Developer</p>
+        </div>
+        <div class="col-12 col-sm-4 text-center">
+          <div>
+            <img class="rounded-circle w-100" style="max-width: 200px" src="https://scontent.fopo1-1.fna.fbcdn.net/v/t1.0-9/15894246_1495739093773554_7807723144081266112_n.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=4KRG-PzZVnYAX_6aTAk&_nc_ht=scontent.fopo1-1.fna&oh=eb8548276140cd8389c1883a07f46d18&oe=5F585792" alt="">
+          </div>
+          <h3>José Joaquim Azevedo</h3>
+          <p>Web Developer</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>

5638b52a778ecd708e1ff47cf6df2255bc557bb1
jose <josejoaquimrizzo@hotmail.com>
2020-08-21 20:00:05 +0100

    Pet class

---
+++ database/db_petfinder_sql/Pet.class.php
@@ -0,0 +1,93 @@
+<?php
+if (!file_exists('./bd.php')) {
+  require('./database/bd.php');
+} else if (file_exists('./bd.php')) {
+  require('./bd.php');
+}
+class Pet extends Conn
+{
+  public $pet_id;
+  public $pet_name;
+  public $pet_image;
+  public $user_id;
+  public $status;
+  public $pet_type;
+  public $description;
+  public $create_at;
+  public $update_at;
+  public $sql;
+  public $stmt;
+  public $e;
+
+  
+
+  public function create()
+  {
+    $this->connect(); //colocar session no is set?
+    if (isset($_POST['pet_name'], $_POST['pet_image'], $_POST['pet_type'], $_POST['description']) && $this->conn != null) {
+      try {
+        $this->pet_name = $_POST['pet_name'];
+        $this->user_email = $_POST['email'];
+        $this->pet_type = $_POST['pet_type'];
+        //Ver se o nome do form consta mesmo como pet_foto!!!
+        $this->pet_image = strlen($_POST['pet_image']) == 0 ? 'n/a' : $_POST['pet_image'];
+        $this->description = $_POST['description'];
+        $this->sql = "INSERT INTO pet (pet_name, pet_image, user_id, pet_type ,description) VALUES (:pet_name, :pet_image, :user_id, :pet_type, :description);";
+
+        $this->stmt = $this->conn->prepare($this->sql);
+        $this->stmt->bindValue(':pet_name', $this->pet_name);
+        $this->stmt->bindValue(':pet_image', $this->pet_image);
+        //verificar como fazer a ligação do $user_id com a $session
+        $this->stmt->bindValue(':user_id', $this->user_id);
+        $this->stmt->bindValue(':pet_typer', $this->pet_type);
+        $this->stmt->bindValue(':description', $this->description);
+        $this->stmt->execute();
+      } catch (PDOException $e) {
+        echo "<script>alert('Erro ao cadastrar!' ERRO - {$e->getMessage()});
+          window.location.href = '../index.php?p=Cadastro'; 
+          </script>";
+      }
+    } else {
+      "<script>window.location.href = '../index.php?p=Cadastro';</script>";
+    }
+  } //como alterar esse método? precisa?
+  public function selectAllInFrontEnd($table)
+  {
+    $this->connectInFrontEnd();
+    try {
+      $this->stmt = $this->conn->prepare("SELECT * FROM $table");
+      $this->stmt->execute();
+    } catch (Exception $e) {
+      echo "<script>alert('Erro ao listar!' ERRO - {$e->getMessage()});
+      window.location.href = '../index.php'; 
+      </script>";
+    }
+  } //como vai aparecer no frontend?
+  // fazer os selects!!!
+  public function selectWhereInFrontEnd($table, $column, $value)
+  {
+    $this->connectInFrontEnd();
+    try {
+      $this->stmt = $this->conn->prepare("SELECT * FROM $table WHERE $column=:value");
+      $this->stmt->bindValue(':value', $value);
+      $this->stmt->execute();
+    } catch (Exception $e) {
+      echo "<script>alert('Erro ao listar!' ERRO - {$e->getMessage()});
+        window.location.href = '../index.php'; 
+      </script>";
+    }
+  }
+  public function delete($table, $column, $value)
+  {
+    $this->connect();
+    try {
+      $this->stmt = $this->conn->prepare("DELETE FROM $table WHERE $column=:value");
+      $this->stmt->bindValue(':value', $value);
+      $this->stmt->execute();
+    } catch (PDOException $e) {
+      echo "<script>alert('Erro ao deletar!' ERRO - {$e->getMessage()});
+        window.location.href = '../index.php?pg=TableUser'; 
+        </script>";
+    }
+  }
+}
--- database/selectall.php
+++ database/selectall.php
@@ -1,8 +1,40 @@
-<?php
+<?php /* ------------> parei aki!!!
 include('bd.php');
 $stmt = $conn->query("SELECT * FROM users");
 while ($row = $stmt->fetch()) {
-echo $row['nome_aluno']. " em ".$row['contacto']."<br>";
+echo $row['pet_name']. " em ".$row['contacto']."<br>";
 echo $row['morada']."<br />\n";
 echo "<hr><br><br>";
 }
+include('bd.php');
+*/
+?>
+<?php
+
+include('bd.php');
+/*If there are no variables going to be used in the query, we can use a conventional query() method instead of prepare and execute.
+
+// select all users
+$stmt = $pdo->query("SELECT * FROM users");
+ the most prefered way to fetch multiple rows which would to be shown on a web-page is calling the 
+ great helper method called fetchAll(). It will put all the rows returned by a query into a PHP array, that later can be used to output 
+ the data using a template (which is considered much better than echoing the data right during the 
+ fetch process). 
+*/
+
+//   https://phpdelusions.net/pdo_examples/select
+
+try {
+
+  $stmt = $conn->prepare("SELECT * FROM pet WHERE status= 'ativo';");
+  $stmt->execute();
+  $petArray = $stmt->fetchAll();
+
+  
+  
+} catch(PDOException $e) {
+  echo "Erro: " . $e->getMessage();
+}
+$conn = null;
+
+?> 

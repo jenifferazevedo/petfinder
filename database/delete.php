f43a9d8a39ededdbd8f504df1e938eda2e726b5f
Jeniffer <jeniqf@gmail.com>
2020-08-21 15:05:10 +0100

    //Developing the delete

--- database/delete.php
+++ database/delete.php
@@ -1,21 +1,31 @@
 <?php
 session_start();
+$classepg;
+isset($_POST['user']) ? $classepg = require('./User.php') : $classepg = require('./Pet.php');
 if (isset($_POST['user'], $_POST['id']) && isset($_SESSION['petfinder-admin']) && is_array($_SESSION['petfinder-admin'])) {
-  require('./User.php');
-  $novoUser = new User();
+  $user = new User();
   if ($_SESSION['petfinder-admin']['id'] == $_POST['id']) {
-    $novoUser->delete('users', 'user_id', $_POST['id']);
+    $user->delete('user_id', $_POST['id']);
     session_destroy();
-    header('Location: ../index.php?p=Home');
+    header('Location: ../index.php');
   } else {
-    $novoUser->delete('users', 'user_id', $_POST['id']);
+    $user->delete('user_id', $_POST['id']);
     header('Location: ../index.php?pg=TableUser');
   }
-} else if (isset($_POST['user'], $_POST['id']) && isset($_SESSION['petfinder-user']) && is_array($_SESSION['petfinder-user'])) {
-  echo 'deletar usuario?';
+  $user->disconnect();
+} else if (isset($_POST['user'], $_POST['id'], $_POST['status']) && isset($_SESSION['petfinder-user']) && is_array($_SESSION['petfinder-user'])) {
+  $status = $_POST['status'];
+  $novoUser = new User();
+  if ($status == 'ativo') {
+    $status = 'inativo';
+    $novoUser->changeStatus($status, 'user_id', $_POST['id']);
+    session_destroy();
+  }
+  header('Location: ../index.php');
+  $novoUser->disconnect();
 } else if (isset($_GET['DeletePet'])) {
   /*ADMIN - deletar pet
   USER - UPDATE STATUS*/
 } else {
-  header('Location: ../index.php?p=Home');
+  header('Location: ../index.php');
 }

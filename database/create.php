bba213d36c9f31b1f2c8e476ab4cb77d02d0047c
Jeniffer <jeniqf@gmail.com>
2020-08-23 00:27:53 +0100

    //Create improved

--- database/create.php
+++ database/create.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-
+$classepg;
+isset($_POST['user']) ? $classepg = require('./User.php') : $classepg = require('./Pet.class.php');
 if (isset($_POST['user'])) {
-  require('./User.php');
-
   $novoUser = new User();
+  $novoUser->connect();
   $novoUser->create();
   if (isset($_SESSION['petfinder-admin']) && is_array($_SESSION['petfinder-admin'])) {
     header('Location: ../index.php?pg=TableUser');
@@ -14,8 +14,26 @@ if (isset($_POST['user'])) {
     </script>";
   }
   $novoUser->disconnect();
-} else if (isset($_GET['CreatePet'])) {
-  //pet;
+} else if (isset($_POST['pet'])) {
+  $newPet = new Pet();
+  $newPet->connect();
+  if (isset($_SESSION['petfinder-admin']) || isset($_SESSION['petfinder-user'])) {
+    $id = isset($_SESSION['petfinder-admin']) ? $_SESSION['petfinder-admin']['id'] : $_SESSION['petfinder-user']['id'];
+    try {
+      $newPet->create($id);
+      if (isset($_SESSION['petfinder-admin'])) header('Location: ../index.php?pg=TablePet');
+      else {
+        echo "<script>alert('Cadastro realizado com sucesso');
+      window.location.href = '../index.php?s=MeusPets'; 
+      </script>";
+      }
+    } catch (PDOException $e) {
+      echo $e;
+    }
+    $newPet->disconnect();
+  } else {
+    header('Location: ../index.php');
+  }
 } else {
   header('Location: ../index.php?p=Cadastro');
 }

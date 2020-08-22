c546e2ca9f87b2cfe459ef192740e9a7651aece1
Jeniffer <jeniqf@gmail.com>
2020-08-23 00:28:36 +0100

    Index improved

--- index.php
+++ index.php
@@ -17,10 +17,12 @@ if (isset($_GET['p'])) {
     else if ($pg == 'TableUser') include("./contents/userstable.php");
     else if ($pg == 'TablePet') include("./contents/petstable.php");
     else if ($pg == 'Cadastro') include("./contents/cadastrouser.php");
+    else if ($pg == 'CadastroPet') include("./contents/cadastropet.php");
     else if ($pg == 'ChangeUser') include("./contents/userstable.php");
     else if ($pg == 'ChangePet') include("./contents/userstable.php");
     else if ($pg == 'Perfil') include("./contents/viewuser.php");
     else if ($pg == 'Detalhes') include("./contents/viewuser.php");
+    else if ($pg == 'DetalhePet') include("./contents/viewpet.php");
     else include("./contents/home.php");
   } else include("./contents/home.php");
 } else if (isset($_SESSION['petfinder-user']) && is_array($_SESSION['petfinder-user'])) {
@@ -31,6 +33,8 @@ if (isset($_GET['p'])) {
     else if ($pg == 'Adocao') include("./contents/adocao.php");
     else if ($pg == 'Contacto') include("./contents/contacto.php");
     else if ($pg == 'Perfil') include("./contents/viewuser.php");
+    else if ($pg == 'MeusPets') include("./contents/meuspets.php");
+    else if ($pg == 'DetalhePet') include("./contents/viewpet.php");
     else if ($pg == 'CadastroPet') include("./contents/cadastropet.php");
     else include("./contents/home.php");
   } else include("./contents/home.php");
